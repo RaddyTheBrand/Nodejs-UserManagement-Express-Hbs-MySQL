79bab049ac44b39622fa2c4b928c7fe66b09d6f8
Raddy <rx3mer@live.co.uk>
2022-03-10 12:02:55 +0000

    Updated handlebars

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+//const bodyParser = require('body-parser'); // No longer Required
+//const mysql = require('mysql'); // Not required -> moved to userController
 
 require('dotenv').config();
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({extended: true})); // New
+app.use(express.urlencoded({ extended: true })); // New
 
 // Parse application/json
 // app.use(bodyParser.json());
@@ -21,10 +21,10 @@ app.use(express.json()); // New
 app.use(express.static('public'));
 
 // Templating Engine
-app.engine('hbs', exphbs( {extname: '.hbs' }));
-app.set('view engine', 'hbs');
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
 
-// Connection Pool
 // You don't need the connection here as we have it in userController
 // let connection = mysql.createConnection({
 //   host: process.env.DB_HOST,
@@ -32,7 +32,6 @@ app.set('view engine', 'hbs');
 //   password: process.env.DB_PASS,
 //   database: process.env.DB_NAME
 // });
- 
 
 const routes = require('./server/routes/user');
 app.use('/', routes);
